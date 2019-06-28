3fa17f7fd0a32bedfda80df9ac2837f59038480b
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-06-28 20:07:44 +0300

    overlay

--- default.nix
+++ default.nix
@@ -1,4 +1,9 @@
-with import <nixpkgs> {};
+with import <nixpkgs> {
+#add_postfix_test
+  overlays = [
+    (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = "adds"; rev = "80bb0352d287009c8b788aa12538e9c91f38c5c1"; }))
+  ];
+};
 
 with lib;
 
@@ -7,144 +12,6 @@ let
   locale = glibcLocales.override {
       allLocales = false;
       locales = ["en_US.UTF-8/UTF-8"];
-  };
-
-  postfix = stdenv.mkDerivation rec {
-      name = "postfix-${version}";
-      version = "3.4.5";
-      srcs = [
-         ( fetchurl {
-            url = "ftp://ftp.cs.uu.nl/mirror/postfix/postfix-release/official/${name}.tar.gz";
-            sha256 = "17riwr21i9p1h17wpagfiwkpx9bbx7dy4gpdl219a11akm7saawb";
-          })
-       ./patch/postfix/mj/lib
-      ];
-      nativeBuildInputs = [ makeWrapper m4 ];
-      buildInputs = [ db openssl cyrus_sasl icu libnsl pcre ];
-      sourceRoot = "postfix-3.4.5";
-      hardeningDisable = [ "format" ];
-      hardeningEnable = [ "pie" ];
-      patches = [
-       ./patch/postfix/nix/postfix-script-shell.patch
-       ./patch/postfix/nix/postfix-3.0-no-warnings.patch
-       ./patch/postfix/nix/post-install-script.patch
-       ./patch/postfix/nix/relative-symlinks.patch
-       ./patch/postfix/mj/sendmail.patch
-       ./patch/postfix/mj/postdrop.patch
-       ./patch/postfix/mj/globalmake.patch
-      ];
-       ccargs = lib.concatStringsSep " " ([
-          "-DUSE_TLS"
-          "-DHAS_DB_BYPASS_MAKEDEFS_CHECK"
-          "-DNO_IPV6"
-          "-DNO_KQUEUE" "-DNO_NIS" "-DNO_DEVPOLL" "-DNO_EAI" "-DNO_PCRE"
-       ]);
-
-       auxlibs = lib.concatStringsSep " " ([
-           "-lresolv" "-lcrypto" "-lssl" "-ldb"
-       ]);
-      preBuild = ''
-          cp -pr ../lib/* src/global
-          sed -e '/^PATH=/d' -i postfix-install
-          sed -e "s|@PACKAGE@|$out|" -i conf/post-install
-
-          # post-install need skip permissions check/set on all symlinks following to /nix/store
-          sed -e "s|@NIX_STORE@|$NIX_STORE|" -i conf/post-install
-
-          export command_directory=$out/sbin
-          export config_directory=/etc/postfix
-          export meta_directory=$out/etc/postfix
-          export daemon_directory=$out/libexec/postfix
-          export data_directory=/var/lib/postfix
-          export html_directory=$out/share/postfix/doc/html
-          export mailq_path=$out/bin/mailq
-          export manpage_directory=$out/share/man
-          export newaliases_path=$out/bin/newaliases
-          export queue_directory=/var/spool/postfix
-          export readme_directory=$out/share/postfix/doc
-          export sendmail_path=$out/bin/sendmail
-          make makefiles CCARGS='${ccargs}' AUXLIBS='${auxlibs}'
-      '';
-
-      installTargets = [ "non-interactive-package" ];
-      installFlags = [ "install_root=installdir" ];
-
-      postInstall = ''
-          mkdir -p $out
-          cat << EOF > installdir/etc/postfix/main.cf
-          mynetworks = 127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128
-          mailbox_size_limit = 0
-          recipient_delimiter = +
-          message_size_limit = 20480000
-          maillog_file = /dev/stdout
-          relayhost = mail-checker2.intr
-          EOF
-          echo "smtp            25/tcp          mail" >> installdir/etc/services
-          echo "postlog   unix-dgram n  -       n       -       1       postlogd" >> installdir/etc/postfix/master.cf
-          echo "*: /dev/null" >> installdir/etc/aliases
-          mv -v installdir/$out/* $out/
-          cp -rv installdir/etc $out
-          sed -e '/^PATH=/d' -i $out/libexec/postfix/post-install
-          wrapProgram $out/libexec/postfix/post-install \
-            --prefix PATH ":" ${lib.makeBinPath [ coreutils findutils gnugrep ]}
-          wrapProgram $out/libexec/postfix/postfix-script \
-            --prefix PATH ":" ${lib.makeBinPath [ coreutils findutils gnugrep gawk gnused ]}
-          rm -f $out/libexec/postfix/post-install \
-                $out/libexec/postfix/postfix-wrapper \
-                $out/libexec/postfix/postfix-script \
-                $out/libexec/postfix/.post-install-wrapped \
-                $out/libexec/postfix/postfix-tls-script \
-                $out/libexec/postfix/postmulti-script \
-                $out/libexec/postfix/.postfix-script-wrapped
-      '';
-  };
-
-  apacheHttpd = stdenv.mkDerivation rec {
-      version = "2.4.39";
-      name = "apache-httpd-${version}";
-      src = fetchurl {
-          url = "mirror://apache/httpd/httpd-${version}.tar.bz2";
-          sha256 = "18ngvsjq65qxk3biggnkhkq8jlll9dsg9n3csra9p99sfw2rvjml";
-      };
-      outputs = [ "out" "dev" ];
-      setOutputFlags = false; # it would move $out/modules, etc.
-      buildInputs = [ perl zlib nss_ldap nss_pam_ldapd openldap];
-      prePatch = ''
-          sed -i config.layout -e "s|installbuilddir:.*|installbuilddir: $dev/share/build|"
-      '';
-
-      preConfigure = ''
-          configureFlags="$configureFlags --includedir=$dev/include"
-      '';
-
-      configureFlags = [
-          "--with-apr=${apr.dev}"
-          "--with-apr-util=${aprutil.dev}"
-          "--with-z=${zlib.dev}"
-          "--with-pcre=${pcre.dev}"
-          "--disable-maintainer-mode"
-          "--disable-debugger-mode"
-          "--enable-mods-shared=all"
-          "--enable-mpms-shared=all"
-          "--enable-cern-meta"
-          "--enable-imagemap"
-          "--enable-cgi"
-          "--disable-ldap"
-          "--with-mpm=prefork"
-      ];
-
-      enableParallelBuilding = true;
-      stripDebugList = "lib modules bin";
-      postInstall = ''
-          #mkdir -p $doc/share/doc/httpd
-          #mv $out/manual $doc/share/doc/httpd
-          mkdir -p $dev/bin
-          mv $out/bin/apxs $dev/bin/apxs
-      '';
-
-      passthru = {
-          inherit apr aprutil ;
-      };
   };
 
   phpioncubepack = stdenv.mkDerivation rec {
@@ -397,43 +264,9 @@ let
       '';
   };
 
-#http://mpm-itk.sesse.net/
-  apacheHttpdmpmITK = stdenv.mkDerivation rec {
-      name = "apacheHttpdmpmITK";
-      buildInputs =[ apacheHttpd ];
-      src = fetchurl {
-          url = "http://mpm-itk.sesse.net/mpm-itk-2.4.7-04.tar.gz";
-          sha256 = "609f83e8995416c5491348e07139f26046a579db20cf8488ebf75d314668efcf";
-      };
-      configureFlags = [ "--with-apxs2=${apacheHttpd}/bin/apxs" ];
-      patches = [ ./patch/httpd/itk.patch ];
-      postInstall = ''
-          mkdir -p $out/modules
-          cp -pr /tmp/out/mpm_itk.so $out/modules
-      '';
-      outputs = [ "out" ];
-      enableParallelBuilding = true;
-      stripDebugList = "lib modules bin";
-  };
-
-  mjerrors = stdenv.mkDerivation rec {
-      name = "mjerrors";
-      buildInputs = [ gettext ];
-      src = fetchGit {
-              url = "git@gitlab.intr:shared/http_errors.git";
-              ref = "master";
-              rev = "f83136c7e6027cb28804172ff3582f635a8d2af7";
-            };
-      outputs = [ "out" ];
-      postInstall = ''
-             mkdir -p $out/tmp $out/mjstuff/mj_http_errors
-             cp -pr /tmp/mj_http_errors/* $out/mjstuff/mj_http_errors/
-      '';
-  };
-
   rootfs = stdenv.mkDerivation rec {
       nativeBuildInputs = [ 
-         mjerrors
+         mjHttpErrorPages
          phpioncubepack
          php72
          php72Packages.rrd
@@ -464,7 +297,7 @@ let
          export s6portableutils="${s6-portable-utils}"
          export phpioncubepack="${phpioncubepack}"
          export php72="${php72}"
-         export mjerrors="${mjerrors}"
+         export mjerrors="${mjHttpErrorPages}"
          export postfix="${postfix}"
          export libstdcxx="${gcc-unwrapped.lib}"
          echo ${apacheHttpd}
@@ -511,7 +344,7 @@ pkgs.dockerTools.buildLayeredImage rec {
                  perl528Packages.LWP 
                  perl528Packages.ListMoreUtilsXS
                  perl528Packages.LWPProtocolHttps
-                 mjerrors
+                 mjHttpErrorPages
                  glibc
                  gcc-unwrapped.lib
     ];
