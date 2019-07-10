04d09d5c635743df7ee564181153cf3dd43f700c
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-07-10 15:15:57 +0300

    maxLayers ;

--- default.nix
+++ default.nix
@@ -343,6 +343,7 @@ gitAbbrev = firstNChars 8 (getEnv "GIT_COMMIT");
 in 
 
 pkgs.dockerTools.buildLayeredImage rec {
+    maxLayers = 100;
     name = "docker-registry.intr/webservices/apache2-php72";
     tag = if gitAbbrev != "" then gitAbbrev else "latest";
     contents = [ php72 
