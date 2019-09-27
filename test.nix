dbca7d543f84b5054d10235882b637ab38e2a5f9
Oleg Pykhalov <go.wigust@gmail.com>
2019-09-27 23:17:28 +0300

    test.nix: Switch to master branch.

--- test.nix
+++ test.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {
   overlays = [
-    (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = "wip-split"; }))
+    (import (builtins.fetchGit { url = "git@gitlab.intr:_ci/nixpkgs.git"; ref = "master"; }))
   ];
 };
 
