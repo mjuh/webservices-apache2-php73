d81683a7d50b5c544625b847f9d06dae04d56026
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-10-14 14:04:32 +0000

    gifsicle

--- default.nix
+++ default.nix
@@ -44,6 +44,7 @@ pkgs.dockerTools.buildLayeredImage rec {
     coreutils
     libjpeg_turbo
     (optipng.override{ inherit libpng ;})
+    gifsicle
   ]
   ++ collect isDerivation mjperl5Packages
   ++ collect isDerivation phpPackages.php73Packages;
