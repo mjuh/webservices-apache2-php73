d5b202f94cf852f079e3b2ddb1f981a3d88771a9
Андрей Здетовецкий <zdetovetskiy@majordomo.ru>
2019-12-23 23:32:30 +0300

    tests to tests

--- test.nix
+++ test.nix
@@ -13,7 +13,7 @@ let
   domain = "php73.ru";
   phpVersion = "php" + lib.versions.major php73.version
     + lib.versions.minor php73.version;
-  containerStructureTestConfig = ./container-structure-test.yaml;
+  containerStructureTestConfig = ./tests/container-structure-test.yaml;
   image = callPackage ./default.nix { inherit ref; };
 
 in maketestPhp {
@@ -45,7 +45,7 @@ in maketestPhp {
       description = "Copy phpinfo-json.php.";
       action = "succeed";
       command =
-        "cp -v ${./phpinfo-json.php} /home/u12/${domain}/www/phpinfo-json.php";
+        "cp -v ${./tests/phpinfo-json.php} /home/u12/${domain}/www/phpinfo-json.php";
     })
     (dockerNodeTest {
       description = "Fetch phpinfo-json.php.";
@@ -58,7 +58,7 @@ in maketestPhp {
       action = "succeed";
       command = testDiffPy {
         inherit pkgs;
-        sampleJson = (./. + "/${phpVersion}.json");
+        sampleJson = (./tests/. + "/${phpVersion}.json");
         output = "/tmp/xchg/coverage-data/deepdiff.html";
       };
     })
@@ -67,9 +67,9 @@ in maketestPhp {
       action = "succeed";
       command = testDiffPy {
         inherit pkgs;
-        sampleJson = (./. + "/${phpVersion}.json");
+        sampleJson = (./tests/. + "/${phpVersion}.json");
         output = "/tmp/xchg/coverage-data/deepdiff-with-excludes.html";
-        excludes = import ./diff-to-skip.nix;
+        excludes = import ./tests/diff-to-skip.nix;
       };
     })
     (dockerNodeTest {
@@ -77,7 +77,7 @@ in maketestPhp {
       action = "succeed";
       command = testDiffPy {
         inherit pkgs;
-        sampleJson = (./. + "/web34/${phpVersion}.json");
+        sampleJson = (./tests/. + "/web34/${phpVersion}.json");
         output = "/tmp/xchg/coverage-data/deepdiff-web34.html";
       };
     })
@@ -86,16 +86,16 @@ in maketestPhp {
       action = "succeed";
       command = testDiffPy {
         inherit pkgs;
-        sampleJson = (./. + "/web34/${phpVersion}.json");
+        sampleJson = (./tests/. + "/web34/${phpVersion}.json");
         output = "/tmp/xchg/coverage-data/deepdiff-web34-with-excludes.html";
-        excludes = import ./diff-to-skip.nix;
+        excludes = import ./tests/diff-to-skip.nix;
       };
     })
     (dockerNodeTest {
       description = "Copy bitrix_server_test.php.";
       action = "succeed";
       command = "cp -v ${
-          ./bitrix_server_test.php
+          ./tests/bitrix_server_test.php
         } /home/u12/${domain}/www/bitrix_server_test.php";
     })
     (dockerNodeTest {
