88bdd6b7992e044c9fd5a24f7018e377c73bc668
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-23 21:11:50 +0300

    Revert "default.nix: Move extraCommands."
    This reverts commit 8874623026a4980d09d36ac6db2ab2fa78120cb0.

--- default.nix
+++ default.nix
@@ -68,6 +68,7 @@ pkgs.dockerTools.buildLayeredImage rec {
       ru.majordomo.docker.cmd = dockerRunCmd php73DockerArgHints "${name}:${tag}";
       ru.majordomo.docker.exec.reload-cmd = "${apacheHttpd}/bin/httpd -d ${rootfs}/etc/httpd -k graceful";
     };
+  };
     extraCommands = ''
       set -xe
       ls
@@ -80,5 +81,4 @@ pkgs.dockerTools.buildLayeredImage rec {
       ln -s /bin usr/sbin
       ln -s /bin usr/local/bin
     '';
-  };
 }
