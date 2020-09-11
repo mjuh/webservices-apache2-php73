2f65a70f51ade563cc33d41d690cbec7fb8dec5e
Oleg Pykhalov <go.wigust@gmail.com>
2020-09-11 18:33:55 +0300

    test.nix: Pass nixpkgs to image.

--- test.nix
+++ test.nix
@@ -14,7 +14,7 @@ let
   phpVersion = "php" + lib.versions.major php73.version
     + lib.versions.minor php73.version;
   containerStructureTestConfig = ./tests/container-structure-test.yaml;
-  image = callPackage ./default.nix { inherit overlayUrl overlayRef; inherit debug; };
+  image = callPackage ./default.nix { inherit nixpkgs; };
 
 in maketestPhp {
   inherit image;
